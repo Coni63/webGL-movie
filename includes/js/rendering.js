54049ea092e3964e441934879cafde0bdd31d92b
= <=>
2018-03-22 18:27:35 +0100

    fix issue with dropdown. Dropdown completed with all movies. Options added for light/rendering

--- includes/js/rendering.js
+++ includes/js/rendering.js
@@ -6,15 +6,25 @@ var instances = {}; // store obj -> movie
 var meshes = [];
 var octree; 
 var mydata = JSON.parse(data);
+var ambientLight;
+var lights = [];
+var max_movie = 4918;
 
-var raycaster; // = new THREE.Raycaster();
+var raycaster;
 var mouse = new THREE.Vector2();
 var intersected;
 
 var params = {
     color : '#ff0000',
     scale : 1,
-    max_points: 250
+    max_points: 250,
+    ambiente_color : '#999999',
+    spot1_color: '#ffffff',
+    spot2_color: '#11E8BB',
+    spot3_color: '#8200C9',
+    color_dropdown: '#00ff00',
+    max_depth : 100,
+    display_fps : true
 };
 
 init();
@@ -25,7 +35,7 @@ function init() {
     container = document.getElementById("container");
 
     // camera
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 100 );
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, params.max_depth );
     camera.position.set( 0, 0, 40 );
 
     // scene
@@ -36,7 +46,7 @@ function init() {
 
     // mesh
     geometry = new THREE.IcosahedronBufferGeometry( 0.1*params.scale, 0 );
-    for(var i=0; i<4918 ; i++){  //max 4918
+    for(var i=0; i<max_movie ; i++){
         var movie = new Movie( mydata.movie_title[i], mydata.X[i], mydata.Y[i], mydata.Z[i]);
         var mesh = new THREE.Mesh( geometry, new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading : true }) );
             mesh.position.set( movie.x, movie.y, movie.z );
@@ -45,18 +55,15 @@ function init() {
         meshes.push(mesh);
     }
     
-    regenerate();
-
     // lights
-    var ambientLight = new THREE.AmbientLight(0x999999 );
+    ambientLight = new THREE.AmbientLight(params.ambiente_color);
     scene.add(ambientLight);
 
-    var lights = [];
-    lights[0] = new THREE.DirectionalLight( 0xffffff, 1 );
+    lights[0] = new THREE.DirectionalLight( params.spot1_color, 1 );
     lights[0].position.set( 30, 0, 0 );
-    lights[1] = new THREE.DirectionalLight( 0x11E8BB, 1 );
+    lights[1] = new THREE.DirectionalLight( params.spot2_color, 1 );
     lights[1].position.set( 20, 30, 15 );
-    lights[2] = new THREE.DirectionalLight( 0x8200C9, 1 );
+    lights[2] = new THREE.DirectionalLight( params.spot3_color, 1 );
     lights[2].position.set( -20, -30, 15 );
     scene.add( lights[0] );
     scene.add( lights[1] );
@@ -65,10 +72,26 @@ function init() {
 
     // dat.gui
     var gui = new dat.GUI();
-
-    gui.addColor( params, 'color' ) //.onChange( function( value ) {console.log(value);} );
-
-    gui.add( params, 'scale', 0.1, 10 ).step( 0.005 ).onChange( function( value ) {
+    
+    var color = gui.addFolder('Colors');
+    color.addColor( params, 'color' ) //.onChange( function( value ) {console.log(value);} );
+    color.addColor( params, 'color_dropdown' )
+    color.addColor( params, 'ambiente_color' ).onChange( function( value ) {
+        ambientLight.color = new THREE.Color( value );
+    });
+    color.addColor( params, 'spot1_color' ).onChange( function( value ) {
+        lights[0].color = new THREE.Color( value );
+    });
+    color.addColor( params, 'spot2_color' ).onChange( function( value ) {
+        lights[1].color = new THREE.Color( value );
+    });
+    color.addColor( params, 'spot3_color' ).onChange( function( value ) {
+        lights[2].color = new THREE.Color( value );
+    });
+    color.close();
+    
+    var rendering = gui.addFolder('Rendering');
+    rendering.add( params, 'scale', 0.1, 10 ).step( 0.005 ).onChange( function( value ) {
         params.scale = value;
         for (var i = 0; i < scene.children.length; i++){
             if (scene.children[i].type == "Mesh"){
@@ -78,20 +101,28 @@ function init() {
             }
         }
     } );
-
-    gui.add( params, 'max_points', 1, 4918 ).step( 1 ).onChange( function( value ) {
+    
+    rendering.add( params, 'max_points', 1, max_movie ).step( 1 ).onChange( function( value ) {
         regenerate();
     } );
     
+    rendering.add( params, 'max_depth', 1, 100 ).step( 1 ).onChange( function( value ) {
+        camera.far = value;
+        camera.updateProjectionMatrix();
+    } );
+    
+    rendering.add( params, 'display_fps').onChange(function( value ){
+        stats.dom.hidden = !value;
+    });
+    
+    rendering.open();
+        
     var obj = { 'Regenerate' : function(){ 
         regenerate();
     }};
     gui.add(obj,'Regenerate');
 
-//    gui.add( helper, 'visible' ).name( 'Show Flow Map');
-
     gui.open();
-
 
     // stats
     stats = new Stats();
@@ -101,11 +132,12 @@ function init() {
     renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setSize( window.innerWidth, window.innerHeight );
     renderer.setPixelRatio( window.devicePixelRatio );
-    // renderer.setClearColor(0xffffff, 0.1);
     container.appendChild( renderer.domElement );
+    
+    regenerate();
 
     // mouse control
-    controls = new THREE.OrbitControls( camera, renderer.domElement  );
+    controls = new THREE.OrbitControls( camera, domElement = renderer.domElement, localElement=renderer.domElement );
 
     // events
     window.addEventListener( 'resize', onWindowResize, false );
@@ -177,11 +209,11 @@ function onDocumentMouseMove( event ) {
 
         intersected.material.color.set( 0xffffff );
         intersected = null;
-        $("#info").text("Select a Movie");
+        $("#info").text("");
         document.body.style.cursor = 'auto';
 
     } else {
-        $("#info").text("Select a Movie");
+        $("#info").text("");
     }
 
 }
@@ -192,6 +224,8 @@ function regenerate(){
             scene.remove(scene.children[i]);
         }
     }
+    
+    $("#movie_list").find('option').remove().end();
     
     octree = new THREE.Octree( {
         //scene: scene,
@@ -204,6 +238,7 @@ function regenerate(){
     for (var i=0; i< params.max_points ; i++){
         scene.add(meshes[i]);
         octree.add(meshes[i], { useFaces: false } );
+        $("#movie_list").append('<option value='+meshes[i].uuid+'>'+ instances[meshes[i].uuid].title + '</option>');
     }
 }
 
@@ -217,3 +252,20 @@ function onKeyDown ( event ) {
     }
 
 }
+
+$( "#movie_list" ).change(function() {
+  $( "select option:selected" ).each(function() {
+     let uuid_selected =  $(this).attr("value");
+      for (var i = 0; i < scene.children.length; i++){
+        if (scene.children[i].uuid == uuid_selected){
+            target_pos = scene.children[i].position;
+            console.log(target_pos);
+            controls.target.set(target_pos.x, target_pos.y, target_pos.z);
+            camera.position.set(target_pos.x+5, target_pos.y, target_pos.z);
+            controls.update();
+            scene.children[i].material.color.set( params.color_dropdown );
+            $("#info").text($(this).text());
+        }
+    }
+  });
+});
