9da402ac503f3840ff093400d5c210aea42bf40f
= <=>
2018-04-06 19:23:15 +0200

    add centroides calculation / display on V4

--- includes/js/rendering_spherical_inside.js
+++ includes/js/rendering_spherical_inside.js
@@ -1,48 +1,32 @@
 var container, stats;
 var camera, scene, renderer;
-var light1, light2;
-var labelPos;
-var sphere;
+var camera_pos = new THREE.Vector3( 0, 0, 0.01 );
 var mouse;
-var mouseXOnMouseDown = 0;
-var mouseYOnMouseDown = 0;
+var labelPos;
 var raycaster;
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
-var deltaRotationQuaternion;
-var data, classNames;
 var points;
 var stats;
 var max_movie = 9150;
-var raycaster;
+var nb_labels = 76;
 var intersected;
 var instances = [];
 var toggle = 0;
 var clock;
-var params = {
-    sphere_color : '#8e0909',
-    ambiente_color : '#999999',
-    spot1_color: '#ffffff',
-    spot2_color: '#11E8BB',
-    spot3_color: '#8200C9',
-    spot4_color: '#ff00ff',
-    display_fps : true,
-    display_sphere : true,
-}
-var scope;
-var pressed = false;
-var camera_pos = new THREE.Vector3( 0, 0, 0.01 );
 var text_container;
+var centroides;
+var mydata;
+var labels;
 
 function init() {
-    
-    var mydata = JSON.parse(data);
+        
+    mydata = JSON.parse(data);
+    centroides = JSON.parse(cluster);
     
     text_container = document.getElementById("textbox_container");
 
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.01, 1.5 );    
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.01, 3 );    
     
     clock = new THREE.Clock();
     clock.start()
@@ -50,7 +34,8 @@ function init() {
     var geometry = new THREE.BufferGeometry();
     var positions = new Float32Array( max_movie*3 );
     var colors = new Float32Array( max_movie*3 );
-
+    
+    var rad = [];
     for ( var i = 0; i < max_movie; ++i ) {      
         var movie = new MovieV2(i, mydata.score[i], mydata.color[i], mydata.primaryTitle[i], mydata.tconst[i], mydata.X[i], mydata.Y[i], mydata.Z[i]);
         instances.push(movie);
@@ -64,6 +49,8 @@ function init() {
         colors[ 3 * i ] = movie.color.r;
         colors[ 3 * i + 1 ] = movie.color.g;
         colors[ 3 * i + 2 ] = movie.color.b;
+        
+        rad.push(movie.score*1000);
         
         $("#movie_list").append('<option value='+i+'>'+ movie.title + '</option>');
     } 
@@ -86,6 +73,14 @@ function init() {
     points = new THREE.Points( geometry, pMaterial );
     scene.add( points );
     
+    // Centroides
+    labels = new THREE.Object3D();
+    for (var i = 0; i < nb_labels; i++) {
+        var label = makeTextSprite( centroides["Style"][i], 5, { r:255, g:0, b:0, a:1.0 } );        
+            label.position.set( centroides["X"][i], centroides["Y"][i], centroides["Z"][i] );
+        labels.add(label)        
+    }
+    scene.add( labels );
     
     renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true, preserveDrawingBuffer: true } );
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -140,7 +135,7 @@ function onDocumentMouseMove( event ) {
 function display_label(){ 
     toggle += clock.getDelta();
 
-    if (camera.zoom < 20) {
+    if (camera.zoom < 10) {
         text_container.innerHTML = "";
         return;
     }
@@ -163,7 +158,6 @@ function display_label(){
         if (d < 1e-3){
             var textbox = new TextboxV2(instances[i].title);
                 textbox.updatePosition(camera, instances[i].vec);
-//            instances[i].textbox.updatePosition(camera, instances[i].vec);
             text_container.appendChild(textbox.element);
         }
     }
@@ -180,7 +174,13 @@ function onDocumentMouseWheel( event ) {
 }
 
 function animate() {
-
+    console.log(camera.zoom);
+    if (camera.zoom > 10){
+        labels.visible = false;
+    } else {
+        labels.visible = true;
+    }
+    
     renderer.render( scene, camera );    
         
     requestAnimationFrame( animate );
@@ -200,7 +200,7 @@ function updateLink(movie) {
 function update_rendering(){
     controls.rotateSpeed = 1/camera.zoom;
     let factor_particle_size = (0.007*camera.zoom+0.14)/49;
-    points.material.size = factor_particle_size;
+    points.material.size = factor_particle_size; 
     camera.updateProjectionMatrix();
     controls.update();
 }
@@ -230,6 +230,126 @@ function onKeyDown ( event ) {
 
 }
 
+function makeTextSprite( message, weight, color ) {
+    var fontface = "Monospace";
+    var fontsize = 30;
+    var borderThickness = 0;
+    var borderColor = color;
+    var backgroundColor = { r:255, g:255, b:255, a:1.0 };
+    var textColor = color;
+
+    var canvas = document.createElement('canvas');
+    canvas.width = 512
+    canvas.height = 128
+    var context = canvas.getContext('2d');
+    context.font = fontsize + "px " + fontface;
+    var metrics = context.measureText( message );
+    var textWidth = metrics.width;
+
+    context.fillStyle = "rgba("+textColor.r+", "+textColor.g+", "+textColor.b+", 1.0)";
+    context.fillText( message, (canvas.width/2) - (textWidth / 2), 60);
+    context.textBaseline = 'middle';
+    context.textAlign = "center";
+
+    var texture = new THREE.Texture(canvas) 
+    texture.needsUpdate = true;
+
+    var spriteMaterial = new THREE.SpriteMaterial( { map: texture} );
+    spriteMaterial.depthWrite = false;
+    spriteMaterial.depthTest = false;
+        
+    var sprite = new THREE.Sprite( spriteMaterial );
+    sprite.scale.set( 0.002*weight * fontsize, 0.001*weight * fontsize, 0.01 * fontsize);
+    
+    return sprite;  
+}
+
+var supportsES6 = function() {
+    try {
+        new Function("(a = 0) => a");
+        return true;
+    }
+    catch (err) {
+        return false;
+    }}();
+
+if (supportsES6 == true){
+
+    var MovieV2 = class {
+        constructor(rank, score, color, title, id, x, y, z){
+            this.rank = rank;
+            this.score = score;
+            this.movieID = id;
+            this.title = title;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.color = new THREE.Color( color );
+            this.vec = new THREE.Vector3(x, y, z);
+        }
+    };
+
+    var TextboxV2 = class {
+        constructor(text) {
+            this.element = document.createElement('div');
+            this.element.className = 'text-label';
+            this.element.style.position = 'absolute';
+            this.element.style.width = 100;
+            this.element.style.height = 100;
+            this.element.innerHTML = text;
+            this.element.style.top = -1000;
+            this.element.style.left = -1000;
+            this.element.style.fontSize = "10px";
+        };
+
+        updatePosition(cam, point) {     
+            var vector = point.clone();
+                vector.project(camera);
+
+            vector.x = (+vector.x + 1)/2 * window.innerWidth;
+            vector.y = (-vector.y + 1)/2 * window.innerHeight;
+            this.element.style.left = vector.x + 'px';
+            this.element.style.top = vector.y + 'px';
+        };
+
+    };
+
+} else {
+
+    function MovieV2(rank, score, color, title, id, x, y, z){
+        this.rank = rank;
+        this.score = score;
+        this.movieID = id;
+        this.title = title;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.color = new THREE.Color( color );
+        this.vec = new THREE.Vector3(x, y, z);
+    }
+
+    function TextboxV2(text) {
+        this.element = document.createElement('div');
+        this.element.className = 'text-label';
+        this.element.style.position = 'absolute';
+        this.element.style.width = 100;
+        this.element.style.height = 100;
+        this.element.innerHTML = text;
+        this.element.style.top = -1000;
+        this.element.style.left = -1000;
+        this.element.style.fontSize = "10px";
+    }
+
+    TextboxV2.prototype.updatePosition = function(cam, point) {     
+        var vector = point.clone();
+            vector.project(camera);
+
+        vector.x = (+vector.x + 1)/2 * window.innerWidth;
+        vector.y = (-vector.y + 1)/2 * window.innerHeight;
+        this.element.style.left = vector.x + 'px';
+        this.element.style.top = vector.y + 'px';
+    }
+}
 
 init();
 animate();
